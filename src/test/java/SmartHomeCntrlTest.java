010aaa2e2a397c13896bb5ed3934bfe39cf4e522
Ali Tabatabaei <a.tabatabaei97@gmail.com>
2018-11-28 02:54:45 +0330

    gas and smoke method CACC completed

--- src/test/java/SmartHomeCntrlTest.java
+++ src/test/java/SmartHomeCntrlTest.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class SmartHomeCntrlTest {
-    SecurityConfig securityConfig;
-    EnvState envState;
-    SmartHomeController smartHomeController;
+    private SecurityConfig securityConfig;
+    private EnvState envState;
+    private SmartHomeController smartHomeController;
 
 
     @Before
@@ -337,6 +337,392 @@ public class SmartHomeCntrlTest {
         this.smartHomeController.fire(0);
         HomeState actual = this.smartHomeController.getHomeState();
         HomeState unexpected = new HomeState(Windows.Open, Doors.Open, Lights.Off, Alarm.Off);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void smoke_IF1_CACC_test1() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Day);
+        this.smartHomeController.smoke(-10);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState expected = new HomeState(Windows.Open, Doors.Close, Lights.Off, Alarm.Off);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void smoke_IF1_CACC_test2() {
+        this.securityConfig.setSecurityMode(SecurityMode.Vacation_Mode);
+        this.smartHomeController.setCurrentTime(Time.Day);
+        this.smartHomeController.smoke(-10);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Close, Lights.Off, Alarm.Off);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void smoke_IF1_CACC_test3() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.envState.setGasLevel(0);
+        this.envState.setFireLevel(0);
+        this.smartHomeController.smoke(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Close, Lights.Off, Alarm.Off);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void smoke_IF1_CACC_test4() {
+        this.securityConfig.setSecurityMode(SecurityMode.Vacation_Mode);
+        this.smartHomeController.setCurrentTime(Time.Day);
+        this.smartHomeController.smoke(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Close, Lights.Off, Alarm.Off);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void smoke_IF2_CACC_test1() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Day);
+        this.smartHomeController.smoke(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState expected = new HomeState(Windows.Open, Doors.Close, Lights.Emergency, Alarm.Off);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void smoke_IF2_CACC_test2() {
+        this.securityConfig.setSecurityMode(SecurityMode.Thief_Mode);
+        this.smartHomeController.setCurrentTime(Time.Day);
+        this.smartHomeController.smoke(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Close, Lights.Emergency, Alarm.Off);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void smoke_IF2_CACC_test3() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.smartHomeController.smoke(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Close, Lights.Emergency, Alarm.Off);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void smoke_IF3_CACC_test1() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.smartHomeController.smoke(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState expected = new HomeState(Windows.Close, Doors.Close, Lights.Off, Alarm.Off);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void smoke_IF3_CACC_test2() {
+        this.securityConfig.setSecurityMode(SecurityMode.Vacation_Mode);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.smartHomeController.smoke(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Close, Doors.Close, Lights.Off, Alarm.Off);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void smoke_IF3_CACC_test3() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Day);
+        this.smartHomeController.smoke(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Close, Doors.Close, Lights.Off, Alarm.Off);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void smoke_IF4_CACC_test1() {
+        this.securityConfig.setSecurityMode(SecurityMode.Thief_Mode);
+        this.securityConfig.setSensitivity(4);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.smartHomeController.smoke(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState expected = new HomeState(Windows.Open, Doors.Open, Lights.Emergency, Alarm.Off);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void smoke_IF4_CACC_test2() {
+        this.securityConfig.setSecurityMode(SecurityMode.Vacation_Mode);
+        this.securityConfig.setSensitivity(4);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.smartHomeController.smoke(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Open, Lights.Emergency, Alarm.Off);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void smoke_IF4_CACC_test3() {
+        this.securityConfig.setSecurityMode(SecurityMode.Thief_Mode);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.smartHomeController.smoke(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Open, Lights.Emergency, Alarm.Off);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void smoke_IF4_CACC_test4() {
+        this.securityConfig.setSecurityMode(SecurityMode.Thief_Mode);
+        this.securityConfig.setSensitivity(4);
+        this.smartHomeController.setCurrentTime(Time.Day);
+        this.smartHomeController.smoke(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Open, Lights.Emergency, Alarm.Off);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void smoke_IF4_CACC_test5() {
+        this.securityConfig.setSecurityMode(SecurityMode.Thief_Mode);
+        this.securityConfig.setSensitivity(4);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.smartHomeController.smoke(-10);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Open, Lights.Emergency, Alarm.Off);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void smoke_IF5_CACC_test1() {
+        this.securityConfig.setSecurityMode(SecurityMode.Vacation_Mode);
+        this.smartHomeController.smoke(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState expected = new HomeState(Windows.Open, Doors.Close, Lights.Emergency, Alarm.On);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void smoke_IF5_CACC_test2() {
+        this.securityConfig.setSecurityMode(SecurityMode.Thief_Mode);
+        this.smartHomeController.smoke(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Close, Lights.Emergency, Alarm.On);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void gas_IF1_CACC_test1() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Day);
+        this.smartHomeController.gas(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState expected = new HomeState(Windows.Open, Doors.Close, Lights.Off, Alarm.Off);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void gas_IF1_CACC_test2() {
+        this.securityConfig.setSecurityMode(SecurityMode.Vacation_Mode);
+        this.smartHomeController.setCurrentTime(Time.Day);
+        this.smartHomeController.gas(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Close, Lights.Off, Alarm.Off);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void gas_IF1_CACC_test3() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.smartHomeController.gas(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Close, Lights.Off, Alarm.Off);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void gas_IF1_CACC_test4() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.smartHomeController.gas(-10);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState expected = new HomeState(Windows.Open, Doors.Close, Lights.Off, Alarm.Off);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void gas_IF2_CACC_test1() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.securityConfig.setSensitivity(3);
+        this.smartHomeController.gas(-10);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState expected = new HomeState(Windows.Open, Doors.Close, Lights.Off, Alarm.On);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void gas_IF2_CACC_test2() {
+        this.securityConfig.setSecurityMode(SecurityMode.Vacation_Mode);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.securityConfig.setSensitivity(3);
+        this.smartHomeController.gas(-10);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Close, Lights.Off, Alarm.On);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void gas_IF2_CACC_test3() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Day);
+        this.securityConfig.setSensitivity(3);
+        this.smartHomeController.gas(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState expected = new HomeState(Windows.Open, Doors.Close, Lights.Off, Alarm.On);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void gas_IF2_CACC_test4() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.securityConfig.setSensitivity(3);
+        this.smartHomeController.gas(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Close, Lights.Off, Alarm.On);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void gas_IF2_CACC_test5() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Day);
+        this.smartHomeController.gas(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Close, Lights.Off, Alarm.On);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void gas_IF3_CACC_test1() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.securityConfig.setSensitivity(5);
+        this.smartHomeController.gas(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState expected = new HomeState(Windows.Open, Doors.Open, Lights.Emergency, Alarm.On);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void gas_IF3_CACC_test2() {
+        this.securityConfig.setSecurityMode(SecurityMode.Vacation_Mode);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.securityConfig.setSensitivity(5);
+        this.smartHomeController.gas(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Open, Lights.Emergency, Alarm.On);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void gas_IF3_CACC_test3() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Day);
+        this.securityConfig.setSensitivity(5);
+        this.smartHomeController.gas(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Open, Lights.Emergency, Alarm.On);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void gas_IF3_CACC_test4() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.smartHomeController.gas(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Open, Lights.Emergency, Alarm.On);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void gas_IF3_CACC_test5() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.securityConfig.setSensitivity(5);
+        this.smartHomeController.gas(-10);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Open, Lights.Emergency, Alarm.On);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void gas_IF4_CACC_test1() {
+        this.securityConfig.setSecurityMode(SecurityMode.Vacation_Mode);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.smartHomeController.gas(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState expected = new HomeState(Windows.Open, Doors.Close, Lights.Emergency, Alarm.Off);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void gas_IF4_CACC_test2() {
+        this.securityConfig.setSecurityMode(SecurityMode.Normal_Mode);
+        this.smartHomeController.setCurrentTime(Time.Night);
+        this.smartHomeController.gas(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Close, Lights.Emergency, Alarm.Off);
+
+        assertNotEquals(unexpected, actual);
+    }
+
+    @Test
+    public void gas_IF4_CACC_test3() {
+        this.securityConfig.setSecurityMode(SecurityMode.Vacation_Mode);
+        this.smartHomeController.setCurrentTime(Time.Day);
+        this.smartHomeController.gas(0);
+        HomeState actual = this.smartHomeController.getHomeState();
+        HomeState unexpected = new HomeState(Windows.Open, Doors.Close, Lights.Emergency, Alarm.Off);
 
         assertNotEquals(unexpected, actual);
     }
